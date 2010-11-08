6bfec9f10071e6d92eeee96b26d948bf0836e8b5
cninja <cninja@gmail.com>
2010-11-08 15:28:48 -0600

    I think I made a faster distinct funciton

---
+++ src/test_project/distinct.clj
@@ -0,0 +1,27 @@
+					;Is this really that much faster?
+
+(defn distinct3
+  [coll]
+  (lazy-seq
+   ((fn step [[f :as xs] seen]
+      (when-let [s (seq xs)]
+	 (if (contains? seen f)
+	   (recur (rest xs) seen)
+	   (cons f (step (rest xs) (conj seen f))))))
+    coll #{})))
+
+(def l (repeat 1000000 [:a :b :c :a :b :d :d :b :e :a :f]))
+(do
+  (println "start")
+  (time (dotimes [n 1000000] (distinct3 l)))
+  (time (dotimes [n 1000000] (distinct l)))
+  (time (dotimes [n 1000000] (distinct3 l)))
+  (time (dotimes [n 1000000] (distinct l)))
+  (time (dotimes [n 1000000] (distinct3 l)))
+  (time (dotimes [n 1000000] (distinct l)))
+  (time (dotimes [n 1000000] (distinct3 l)))
+  (time (dotimes [n 1000000] (distinct l)))
+  (time (dotimes [n 1000000] (distinct3 l)))
+  (time (dotimes [n 1000000] (distinct l)))
+
+  )
